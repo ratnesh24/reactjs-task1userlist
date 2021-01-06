ca4c8c31f119ec5668b6408735cb51d40e5da50c
ratnesh24 <ratnesh@siliconithub.com>
2021-01-06 17:08:47 +0530

    2 files modified

--- App.js
+++ App.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import './css/App.scss';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './Home';
+import React from "react";
+import "./css/App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./Home";
 //import UserList from './UserList';
-import UserListTop from './UserListTop'
-import UserDetail from './UserDetail';
-import Error404 from './404';
+import UserListTop from "./UserListTop";
+import UserDetail from "./UserDetail";
+import Error404 from "./404";
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact default strict path="/home" component={Home} />
+        {/* <Route exact path="/home" component={Home} /> */}
         <Route exact path="/users" component={UserListTop} />
         <Route exact path="/users/:userID" component={UserDetail} />
 
         <Route exact path="/404" component={Error404} />
       </Switch>
-      {/* <Redirect to="/404" /> */}
+      <Redirect to="/home" />
       <Footer />
-    </Router >
+    </Router>
   );
 }
 
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,55 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+//import PropTypes from 'prop-types';
 
 // Name
 let name = {
-    fName: 'Ratnesh',
-    lName: 'Sharma'
+  fName: "Ratnesh",
+  lName: "Sharma"
 };
-let fullName = name.fName + ' ' + name.lName;
+let fullName = name.fName + " " + name.lName;
 
 // Date & Time
-let newDate = new Date()
-let currHours = newDate.getHours()
+let newDate = new Date();
+let currHours = newDate.getHours();
 
 // Greetings
-let greetings = ""
+let greetings = "";
 if (currHours > 1 && currHours < 12) {
-    greetings = "Good Morning"
+  greetings = "Good Morning";
 } else if (currHours >= 12 && currHours < 16) {
-    greetings = "Good Noon"
+  greetings = "Good Noon";
 } else if (currHours >= 16 && currHours < 20) {
-    greetings = "Good Everning"
+  greetings = "Good Everning";
 } else if (currHours >= 20 && currHours <= 24) {
-    greetings = "Good Night"
+  greetings = "Good Night";
 }
 
-let welcomeMsg = ""
-
-function Header(props) {
-    const isLoggedIn = props.isLoggedIn;
-
-    if (isLoggedIn) {
-        welcomeMsg = 'Hi ' + fullName
-    }
-    else {
-        welcomeMsg = 'Hi Guest'
-    }
-
+//let welcomeMsg = ""
+class Header extends React.Component {
+  render() {
     return (
-        <>
-            <header>
-                <h1 className="logo">{`${welcomeMsg}, ${greetings} `}</h1>
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/users">User List</Link></li>
-                    </ul>
-                </nav>
-            </header >
-            <div className="clearfix"></div>
-        </>
+      <>
+        <header>
+          <h1 className="logo">{`${fullName}, ${greetings} `}</h1>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/home">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/users">User List</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div className="clearfix" />
+      </>
     );
+  }
 }
 
 export default Header;
