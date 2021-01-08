ed5e20091107201d4e70ee32a65ae6afc331eadd
ratnesh24 <ratnesh@siliconithub.com>
2021-01-08 11:13:00 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-function tick() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-setInterval(tick, 1000);
+// function tick() {
+
+// }
+
+// setInterval(tick, 1000);
