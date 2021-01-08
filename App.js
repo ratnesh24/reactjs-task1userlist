a825afcc289b51b3b1ad30d5e51e82fa3de3d836
ratnesh24 <ratnesh@siliconithub.com>
2021-01-08 13:52:38 +0530

    1 file modified

--- App.js
+++ App.js
@@ -1,50 +1,45 @@
-import React from 'react';
-import './css/App.scss';
-import Login from './components/Login';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import UserListTop from './components/UserListTop'
-import UserDetail from './components/UserDetail';
-import Home from './Home';
-import Error404 from './404';
+import React from "react";
+import "./css/App.scss";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import UserListTop from "./components/UserListTop";
+import UserDetail from "./components/UserDetail";
+import Home from "./Home";
+import Error404 from "./404";
 //import Protected from './components/Protected';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-  let isLogin = JSON.parse(localStorage.getItem('isLogin'))
+  let isLogin = JSON.parse(localStorage.getItem("isLogin"));
   return (
     <BrowserRouter>
       <Header />
       <Switch>
-        {
-          isLogin ?
-            <>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/users" component={UserListTop} />
-              <Route exact path="/users/:userID" component={UserDetail} />
-              {/* <Route exact path="/users">
+        {isLogin ? (
+          <>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/users" component={UserListTop} />
+            <Route exact path="/users/:userID" component={UserDetail} />
+            {/* <Route exact path="/users">
                 <Protected comp={UserListTop} />
               </Route>
               <Route exact path="/users/:userID">
                 <Protected comp={UserDetail} />
                 <UserDetail />
               </Route> */}
-              <Route exact path="/404" component={Error404} />
-            </>
-            :
-            <>
-              <Route exact default path="/" component={Login} />
-              {/* <Route exact default strict path="/login" component={Login} /> */}
-            </>
-        }
-
+            <Route exact path="/404" component={Error404} />
+          </>
+        ) : (
+          <>
+            <Route exact default path="/" component={Login} />
+            {/* <Route exact default strict path="/login" component={Login} /> */}
+          </>
+        )}
       </Switch>
       <Footer />
-    </BrowserRouter >
+    </BrowserRouter>
   );
 }
 
