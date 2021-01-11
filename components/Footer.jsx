6a293fb521557468afb3236a77cc874d3958ff6a
ratnesh24 <ratnesh@siliconithub.com>
2021-01-11 12:38:21 +0530

    2 files modified

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Date & Time
 let newDate = new Date()
@@ -6,17 +6,33 @@ let date = newDate.getDate()
 let month = newDate.getMonth()
 const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 let year = newDate.getFullYear()
+//let time = new Date().toLocaleTimeString()
+let curdate = date + ' ' + monthName[month] + ', ' + year
 
-function Footer() {
+const Footer = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        var timerID = setInterval(() => tick(), 1000);
+
+        return function cleanup() {
+            clearInterval(timerID);
+        };
+    });
+    function tick() {
+        setDate(new Date());
+    }
     return (
         <>
             <footer>
-                <div className="date">
-                    {`${date} ${monthName[month]}, ${year} / ${new Date().toLocaleTimeString()}`}
-                </div>
+                <div className="date" >{curdate + ' / ' + date.toLocaleTimeString()}</div>
             </footer >
         </>
     );
 }
 
+// function tick() {
+// }
+
 export default Footer;
+
